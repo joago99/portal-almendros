4e796a7f7eab1baec23414f6b6b90dcb441cd978
Joaquin Almendra <joaquinalmendra@benzinga.com>
2026-06-30 00:14:28 -0400

    Cleanup: remove temp debug files

--- scripts/debug_prefix.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once __DIR__ . '/db.php';
-require_once __DIR__ . '/config.php';
-header('Content-Type: application/json');
-session_start();
